34e8f365be6824e3103bd681d7bc4106a1adb645
tylerac7622 <tac7622@g.rit.edu>
2017-02-06 12:43:08 -0500

    Added a basic enemy that follows a hard coded path to the base

---
+++ CasualGame1/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int health;
+    public int speed;
+    public Queue<Vector2> path = new Queue<Vector2>();
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves the enemy to its next point (VERY simple and not optimized)
+        if (path.Peek().x < transform.position.x)
+        {
+            this.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if (path.Peek().x > transform.position.x)
+        {
+            this.transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if (path.Peek().y < transform.position.z)
+        {
+            this.transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+        if (path.Peek().y > transform.position.z)
+        {
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Vector2.Distance(path.Peek(), new Vector2(transform.position.x, transform.position.z)) <= .5f)
+        {
+            path.Dequeue();
+        }
+        //destroy the object if it reached the end of its path (the base)
+        if(path.Count == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //copies a list into a queue
+    public void CopyList(List<Vector2> incoming)
+    {
+        for(int i = 0; i < incoming.Count; i++)
+        {
+            path.Enqueue(incoming[i]);
+        }
+    }
+}
---
+++ CasualGame1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //current wave number
+    private int waveNumber = 1;
+
+    // enemy prefab and spawn point
+    public Vector2 enemySpawnPoint;
+    public GameObject enemyPrefab;
+
+    //starting time until next enemy spawns
+    private float spawnInterval = 0;
+
+    //the path each enemy will follow
+    private List<Vector2> enemyPath;
+
+    //the prefab allowing new towers to be placed
+    public GameObject towerPrefab;
+    //the transparent tower object which is moved around with the mouse
+    public GameObject fakeTower;
+
+    //the size of each interval on the grid
+    public int gridIntervalSize = 10;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //creates the path each enemy will use
+        enemyPath = new List<Vector2>();
+        enemyPath.Add(new Vector2(-5, 45));
+        enemyPath.Add(new Vector2(-5, 35));
+        enemyPath.Add(new Vector2(-5, 25));
+        enemyPath.Add(new Vector2(-5, 15));
+        enemyPath.Add(new Vector2(-5, 5));
+        enemyPath.Add(new Vector2(-5, -5));
+        enemyPath.Add(new Vector2(-5, -15));
+        enemyPath.Add(new Vector2(-5, -25));
+        enemyPath.Add(new Vector2(-5, -35));
+        enemyPath.Add(new Vector2(5, -35));
+        enemyPath.Add(new Vector2(15, -35));
+        enemyPath.Add(new Vector2(25, -35));
+    }
+
+    //spawns a new enemy and initializes its path
+    void SpawnEnemy()
+    {
+        GameObject newEnemy = Instantiate(enemyPrefab, new Vector3(enemySpawnPoint.x, 4.5f, enemySpawnPoint.y), Quaternion.identity);
+        newEnemy.GetComponent<EnemyScript>().CopyList(enemyPath);
+    }
+
+    //moves the transparent tower based on where the mouse is, to show the player where the tower would be placed
+    void MoveFakeTower()
+    {
+        RaycastHit hit;
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool rayCast = Physics.Raycast(mouseRay, out hit);
+        if (rayCast && hit.transform.tag == "Ground")
+        {
+            Vector2 target = new Vector2(gridIntervalSize * Mathf.Floor(hit.point.x / gridIntervalSize) + (gridIntervalSize / 2), gridIntervalSize * Mathf.Floor(hit.point.z / gridIntervalSize) + (gridIntervalSize / 2));
+            fakeTower.transform.position = new Vector3(target.x, 5, target.y);
+            fakeTower.SetActive(true);
+        }
+        else
+        {
+            fakeTower.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //counts down to when the next enemy appears
+        spawnInterval -= Time.deltaTime;
+        if(spawnInterval <= 0)
+        {
+            SpawnEnemy();
+            spawnInterval = 5;
+        }
+
+        MoveFakeTower();
+
+        //places a new tower where the player clicks, if there is nothing there
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            bool rayCast = Physics.Raycast(mouseRay, out hit);
+            if (rayCast && hit.transform.tag == "Ground")
+            {
+                Vector2 target = new Vector2(10 * Mathf.Floor(hit.point.x / 10) + 5, 10 * Mathf.Floor(hit.point.z / 10) + 5);
+                Instantiate(towerPrefab, new Vector3(target.x, 5, target.y), Quaternion.identity);
+            }
+        }
+    }
+}
