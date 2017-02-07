c1b19148f4b1d84cf439887f40912d94b9e93f68
tylerac7622 <tac7622@ad.rit.edu>
2017-02-07 10:59:30 -0500

    Added an ugly but functional UI system and game loop

--- CasualGame1/Assets/Scripts/GameManager.cs
+++ CasualGame1/Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     public GameObject enemyPrefab;
 
     //starting time until next enemy spawns
-    private float spawnInterval = 0;
+    private float spawnInterval = 1;
 
     //the prefab allowing new towers to be placed
     public GameObject towerPrefab;
@@ -24,9 +24,27 @@ public class GameManager : MonoBehaviour
     //the size of each interval on the grid
     public int gridIntervalSize = 10;
 
+    public Canvas UI;
+
+    public Camera playCamera;
+    public Camera buildCamera;
+
+    private enum GameState
+    {
+        BuildPhase,
+        PlayPhase,
+        WinPhase,
+        LosePhase,
+    };
+
+    GameState currentGame = GameState.BuildPhase;
+
     // Use this for initialization
     void Start ()
     {
+        playCamera.gameObject.SetActive(false);
+        buildCamera.gameObject.SetActive(true);
+        UI.transform.FindChild("Start Wave").gameObject.SetActive(true);
     }
 
     //spawns a new enemy and initializes its path
@@ -57,27 +75,86 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-        //counts down to when the next enemy appears
-        spawnInterval -= Time.deltaTime;
-        if(spawnInterval <= 0)
+        if (currentGame == GameState.PlayPhase)
         {
-            SpawnEnemy();
-            spawnInterval = 5;
-        }
-
-        MoveFakeTower();
-
-        //places a new tower where the player clicks, if there is nothing there
-        RaycastHit hit;
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            bool rayCast = Physics.Raycast(mouseRay, out hit);
-            if (rayCast && hit.transform.tag == "Ground")
+            //counts down to when the next enemy appears
+            spawnInterval -= Time.deltaTime;
+            if (spawnInterval <= 0)
             {
-                Vector2 target = new Vector2(10 * Mathf.Floor(hit.point.x / 10) + 5, 10 * Mathf.Floor(hit.point.z / 10) + 5);
-                Instantiate(towerPrefab, new Vector3(target.x, 5, target.y), Quaternion.identity);
+                SpawnEnemy();
+                spawnInterval = 5;
             }
         }
+        if(currentGame == GameState.BuildPhase)
+        {
+                
+            MoveFakeTower();
+
+            //places a new tower where the player clicks, if there is nothing there
+            RaycastHit hit;
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                bool rayCast = Physics.Raycast(mouseRay, out hit);
+                if (rayCast && hit.transform.tag == "Ground")
+                {
+                    Vector2 target = new Vector2(10 * Mathf.Floor(hit.point.x / 10) + 5, 10 * Mathf.Floor(hit.point.z / 10) + 5);
+                    Instantiate(towerPrefab, new Vector3(target.x, 5, target.y), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    public void StartWave()
+    {
+        currentGame = GameState.PlayPhase;
+
+        playCamera.gameObject.SetActive(true);
+        buildCamera.gameObject.SetActive(false);
+        UI.transform.FindChild("Start Wave").gameObject.SetActive(false);
+    }
+
+    public void WinWave()
+    {
+        currentGame = GameState.BuildPhase;
+
+        waveNumber += 1;
+
+        playCamera.gameObject.SetActive(false);
+        buildCamera.gameObject.SetActive(true);
+        UI.transform.FindChild("Start Wave").gameObject.SetActive(true);
+    }
+    public void LoseWave()
+    {
+        currentGame = GameState.LosePhase;
+
+        playCamera.gameObject.SetActive(true);
+        buildCamera.gameObject.SetActive(false);
+        UI.transform.FindChild("Start Wave").gameObject.SetActive(false);
+        UI.transform.FindChild("Lose").gameObject.SetActive(true);
+        UI.transform.FindChild("Restart").gameObject.SetActive(true);
+    }
+    public void WinGame()
+    {
+        currentGame = GameState.WinPhase;
+
+        playCamera.gameObject.SetActive(true);
+        buildCamera.gameObject.SetActive(false);
+        UI.transform.FindChild("Start Wave").gameObject.SetActive(false);
+        UI.transform.FindChild("Restart").gameObject.SetActive(true);
+        UI.transform.FindChild("Win").gameObject.SetActive(true);
+    }
+    public void Restart()
+    {
+        currentGame = GameState.BuildPhase;
+
+        waveNumber = 1;
+
+        playCamera.gameObject.SetActive(false);
+        buildCamera.gameObject.SetActive(true);
+        UI.transform.FindChild("Start Wave").gameObject.SetActive(true);
+        UI.transform.FindChild("Restart").gameObject.SetActive(false);
+        UI.transform.FindChild("Win").gameObject.SetActive(false);
+        UI.transform.FindChild("Lose").gameObject.SetActive(false);
     }
 }
