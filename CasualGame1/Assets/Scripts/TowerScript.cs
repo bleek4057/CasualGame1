46bb4e32c6962008f3b708275070a061ec6c4abc
Grant Parker <gjp8374@rit.edu>
2017-02-10 14:33:20 -0500

    Started enemy scripting

--- CasualGame1/Assets/Scripts/TowerScript.cs
+++ CasualGame1/Assets/Scripts/TowerScript.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class TowerScript : MonoBehaviour {
 
+    public List<EnemyScript> enemies = new List<EnemyScript>();
+
+    private SphereCollider rangeSphere;
+    private float _timer = 0;
+
+    public ParticleSystem ps;
+
 	// Use this for initialization
 	void Start () {
-		
+        ps = this.GetComponent<ParticleSystem>();
+        rangeSphere = this.gameObject.GetComponent<SphereCollider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        _timer -= Time.deltaTime;
+
+        if(_timer <= 0 && enemies.Count > 0)
+        {
+            
+        }
 	}
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if(obj.GetComponent<EnemyScript>() != null)
+        {
+            enemies.Add(obj.GetComponent<EnemyScript>());
+        }
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        if(obj.GetComponent<EnemyScript>() != null)
+        {
+            enemies.Remove(obj.GetComponent<EnemyScript>());
+        }
+    }
 }
