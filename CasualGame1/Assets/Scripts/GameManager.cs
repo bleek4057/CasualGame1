3b558d6b912fadeeb8aa72926b141b7feb767eb2
tylerac13731 <tac7622@g.rit.edu>
2017-02-24 14:18:10 -0500

    Fixed bug

--- CasualGame1/Assets/Scripts/GameManager.cs
+++ CasualGame1/Assets/Scripts/GameManager.cs
@@ -365,7 +365,7 @@ public class GameManager : MonoBehaviour
         playerBase.transform.GetChild(1).gameObject.SetActive(false);
         buildCamera.transform.position = new Vector3(0, 150, 0);
 
-        playerBase.getComponent<BaseScript>().health = 4;
+        playerBase.GetComponent<BaseScript>().health = 4;
 
         UI.transform.FindChild("Help").GetChild(0).gameObject.SetActive(false);
         UI.transform.FindChild("Help").GetChild(1).gameObject.SetActive(false);
